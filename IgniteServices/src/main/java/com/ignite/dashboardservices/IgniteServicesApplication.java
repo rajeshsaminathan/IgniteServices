67528fa72c83cefcf0bc5d94fec14fda9de863b8
rajeshsaminathan <rajeshsaminathan@gmail.com>
2016-10-12 22:52:40 -0500

    IgniteServices
    Ignite services for Dashboard

---
+++ IgniteServices/src/main/java/com/ignite/dashboardservices/IgniteServicesApplication.java
@@ -0,0 +1,12 @@
+package com.ignite.dashboardservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteServicesApplication.class, args);
+	}
+}
