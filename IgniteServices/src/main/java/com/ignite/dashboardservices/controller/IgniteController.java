82bf1121deb7ff94c54eebb734fe854a32194145
Rajesh kumar Saminathan <rajeshsaminathan@gmail.com>
2016-10-20 00:31:46 -0500

    Added Cloud config

--- IgniteServices/src/main/java/com/ignite/dashboardservices/controller/IgniteController.java
+++ IgniteServices/src/main/java/com/ignite/dashboardservices/controller/IgniteController.java
@@ -17,31 +17,43 @@ public class IgniteController {
 	@Autowired
 	ReceivingSrvc receivingDaoSrvc;
 
+	@RequestMapping("/")
+	public ReceivingMetrics helloWorld() {
+		System.out.println("Welcome to Ignite 2.0");
+		return null;
+
+	}
+
 	@RequestMapping("/palletinquiry")
-	public ReceivingMetrics getRcvMetrics(@RequestParam(value = "pallet") String palletId) {
+	public ReceivingMetrics getRcvMetrics(
+			@RequestParam(value = "pallet") String palletId) {
 		return receivingDaoSrvc.getReceivingMetrics(palletId);
-		
-		/*return new SimpleDateFormat("MM/dd/yyyy HH:mm").format(receivingDaoSrvc.getReceivingMetrics(palletId).rcvdDate);*/
+
+		/*
+		 * return new
+		 * SimpleDateFormat("MM/dd/yyyy HH:mm").format(receivingDaoSrvc
+		 * .getReceivingMetrics(palletId).rcvdDate);
+		 */
 	}
 
 	@RequestMapping("/listpallet")
 	public List<ReceivingMetrics> getallRcvMetrics() {
 		return receivingDaoSrvc.getallReceivingMetrics();
-		}
-	
+	}
+
 	@RequestMapping("/slot/SSTK")
 	public List<SlotMetrics> getSSTKSlotMetrics() {
 		return receivingDaoSrvc.getSlotMetrics("SSTK");
-		}
+	}
 
 	@RequestMapping("/slot/PBYL")
 	public List<SlotMetrics> getPBYLSlotMetrics() {
 		return receivingDaoSrvc.getSlotMetrics("PBYL");
-		}
-	
+	}
+
 	@RequestMapping("/slot/EASYPICK")
 	public List<SlotMetrics> getPickSlotMetrics() {
 		return receivingDaoSrvc.getSlotMetrics("EASYPICK");
-		}
+	}
 
 }
